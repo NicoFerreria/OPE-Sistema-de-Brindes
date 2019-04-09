d3af81fc20506b64b37a28e6e2de338b3ef611ac
Nicolas <nicolas_ferreira10@hotmail.com>
2019-04-08 22:26:48 -0300

    Adicionada classe auxiliar utilitarios e implementado o metodo de conexao com o banco de dados

---
+++ OPS_OphellSystem/utilitarios.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data;
+
+namespace OPS_OphellSystem
+{
+    static class utilitarios
+    {
+        static DataTable RealizaConexaoBd(string sqlString)
+        {
+            try
+            {
+                DataTable dtDados = new DataTable();
+                String strConn = @"Data Source=C:\OphellSB\DB\OpehellBase.db";
+                SQLiteConnection conn = new SQLiteConnection(strConn);
+                SQLiteDataAdapter da = new SQLiteDataAdapter(sqlString, strConn);
+                da.Fill(dtDados);
+
+                return dtDados;
+
+            }
+            catch (Exception ex)
+            {
+                throw new System.Exception(ex.Message);
+            }
+        }
+    }
+}
